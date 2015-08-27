ed40896ba15399cce74df5fbdf500dd1c4939f43
alisabzevari <alisabzevari@gmail.com>
2015-08-28 01:05:41 +0430

    Added settings page
    Decided to migrate to materializeCSS

--- src/aurelia/src/views/settings.ts
+++ src/aurelia/src/views/settings.ts
@@ -1,1 +1,14 @@
-export class Settings { }
+export class Settings {
+  public settings: SettingItem[];
+  constructor() {
+    this.settings = [
+      { active: true, path: "c:\\" },
+      { active: false, path: "d:\\Movies" }
+    ];
+  }
+}
+
+export interface SettingItem {
+  active: boolean;
+  path: string;
+}
