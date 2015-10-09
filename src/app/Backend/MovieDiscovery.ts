79a3db599680ccf5837ae5fdd6724824c4fbc92f
Ali Sabzevari <alisabzevari@gmail.com>
2015-10-09 21:19:44 +0330

    Finish implementation of mvoie discovery part and movies

--- src/app/Backend/MovieDiscovery.ts
+++ src/app/Backend/MovieDiscovery.ts
@@ -1,7 +1,6 @@
 import fs = require("fs");
 import path = require("path");
 import {Movie} from "../model/Movie";
-import {OmdbClient} from "./OmdbClient";
 import http = require("http");
 
 export class MovieDiscovery {
--- src/app/Backend/Movies.ts
+++ src/app/Backend/Movies.ts
@@ -3,30 +3,30 @@ import {Movie} from "../model/Movie";
 import fs = require("fs");
 import path = require("path");
 import {SettingsProvider} from "./SettingsProvider";
+import {MovieDiscovery} from "./MovieDiscovery";
 
 @autoinject()
 export class Movies {
   constructor(private settingsProvider: SettingsProvider) { }
 
   public getAll(): Promise<Movie[]> {
-    let result = [];
-    this.settingsProvider.movieFolders.forEach(mf => {
-      if (mf.active) {
-        let paths = fs.readdirSync(mf.path).map(p => path.join(mf.path, p));
-        paths.forEach(p => {
-          if (fs.lstatSync(p).isDirectory()) {
-            var movie = this.getMovieInfo(p);
-            if (movie)
-              result.push();
-             else
-             {
-               
-             }
+    let promises = [];
+    let movieDiscovery = new MovieDiscovery();
+    this.settingsProvider.movieFolders
+      .filter(mf => mf.active)
+      .reduce((prev, mf) => prev.concat(fs.readdirSync(mf.path).map(p => path.join(mf.path, p)).filter(path => fs.lstatSync(path).isDirectory())), [])
+      .forEach(folder => {
+        let promise = new Promise((resolve, reject) => {
+          let movieInfo = this.getMovieInfo(folder);
+          if (movieInfo)
+            resolve(movieInfo);
+          else { 
+            return movieDiscovery.discoverMovie(path.dirname(folder));
           }
         });
-      }
-    });
-    return Promise.resolve(result);
+        promises.push(promise);
+      });
+    return Promise.all(promises);
   }
 
   private getMovieInfo(address: string): Movie {
