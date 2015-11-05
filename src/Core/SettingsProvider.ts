fda246902c2d18f5632f85b89f721857e9b792d2
Ali Sabzevari <alisabzevari@gmail.com>
2015-11-05 17:28:06 +0330

    add saveChanges

--- src/Core/SettingsProvider.ts
+++ src/Core/SettingsProvider.ts
@@ -3,12 +3,28 @@ import fs = require("fs");
 export class SettingsProvider {
   movieFolders: { path: string; active: boolean }[];
   private static fileName = "./settings.json";
+  private settingsFileName: string;
 
   constructor(fileName?: string) {
-    let fname = fileName;
-    if (!fname)
-      fname = SettingsProvider.fileName;
-    var f = JSON.parse(fs.readFileSync(fname).toString());
+    this.settingsFileName = fileName;
+    if (!this.settingsFileName)
+      this.settingsFileName = SettingsProvider.fileName;
+    var f = JSON.parse(fs.readFileSync(this.settingsFileName).toString());
     this.movieFolders = f.movieFolders;
+  }
+
+  saveChanges(): Promise<void> {
+    var promise = new Promise<void>((resolve, reject) => {
+      let settingsObj = {
+        movieFolders: this.movieFolders
+      };
+      fs.writeFile(this.settingsFileName, JSON.stringify(settingsObj), (err) => {
+        if (err)
+          reject(err);
+        else
+          resolve();
+      });
+    });
+    return promise;
   }
 }
