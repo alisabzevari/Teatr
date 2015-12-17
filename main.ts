43601b4d95b5b1ae8fe295b87e8f829c3c674b80
Ali Sabzevari <alisabzevari@gmail.com>
2015-12-17 14:52:45 +0330

    add tsd
    added aurelia typings (but not from tsd because they didn't exist there). Decided to include typings to source control because of this issue.

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 import "materialize";
+import {Aurelia} from "aurelia-framework";
+import {bootstrap} from "aurelia-bootstrapper";
 
-console.log("loaded");
+bootstrap((aurelia: Aurelia) => {
+  aurelia.use
+    .standardConfiguration()
+    .developmentLogging()
+    .feature("resources", {});
+  aurelia.start().then(a => a.setRoot());
+});
