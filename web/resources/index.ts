a1c217bc6b1ff341c070e2f289dd60c3b42046c6
Ali Sabzevari <alisabzevari@gmail.com>
2016-01-03 21:21:03 +0330

    Fix icons problem

--- web/resources/index.ts
+++ web/resources/index.ts
@@ -3,8 +3,8 @@ import {Aurelia} from "aurelia-framework";
 // import "./mat-chk";
 
 export function configure(aurelia: Aurelia) {
-  aurelia.use
-    .globalResources(["materialize", "mat-chk"]);
+//   aurelia.use
+//     .globalResources(["materialize", "mat-chk"]);
 }
 
 // Couldn't use typescript typing because it was different with js library
