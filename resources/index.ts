dadc91a36b7bdbb2ed230ce9c1de58996ea04cd1
Ali Sabzevari <ali@tidm.ir>
2016-04-30 18:14:21 +0430

    From scratch!

--- resources/index.ts
+++
@@ -1,9 +0,0 @@
-import {Aurelia} from "aurelia-framework";
-
-export function configure(aurelia: Aurelia) {
-  // aurelia
-  //   .globalResources("materialize")
-  //   .globalResources("mat-chk");
-}
-
-// Couldn't use typescript typing because it was different with js library
