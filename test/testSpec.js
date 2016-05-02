ae2f86409fcf429de482ee806d12e6bc7385e10e
Ali Sabzevari <ali@tidm.ir>
2016-05-02 14:53:53 +0430

    remove compiled js files

--- test/testSpec.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var chai_1 = require("chai");
-describe("a test", function () {
-    it("should run!", function () {
-        chai_1.expect(1 + 1 == 2, "a simple test");
-    });
-});
